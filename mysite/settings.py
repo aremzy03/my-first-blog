2a7bdd261811e8194758f17a560541d64b88fb7e
aremzy03 <aremzy2018@gmail.com>
2024-04-27 14:30:06 +0100

    just made som improvements to the web pages

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(&6z#_ds*-=0oul%=q%w5sikk%xh_r%ij4hh=pa2vm&km05)ji
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'aremzy03/pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'aremzy03.pythonanywhere.com']
 
 
 # Application definition
